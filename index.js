a8f75998488e02b97e295567c54e880a000ec88e
ssn3192 <shivu.narang@gmail.com>
2016-03-08 23:08:48 -0600

    Hello

--- index.js
+++ index.js
@@ -128,7 +128,6 @@ app.get('/getUniversityList' , function(request, response){
   });
 
 
-});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));

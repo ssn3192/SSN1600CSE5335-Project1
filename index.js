7f6f5a588cdac61cc57203f198bf69a63edf44ad
ssn3192 <shivu.narang@gmail.com>
2016-03-08 22:46:45 -0600

    Hello

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : true}));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -31,7 +35,7 @@ app.get('/getUniversityList' , function(req, res){
   });
 
 
-}
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
